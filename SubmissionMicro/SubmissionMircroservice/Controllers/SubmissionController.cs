1b376e5f802da7654ccc2c8546e40a3c2734e7af
clafollette303 <lafollettec@etsu.edu>
2024-11-16 19:12:36 -0500

    initial commit

---
+++ SubmissionMicro/SubmissionMircroservice/Controllers/SubmissionController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Net.Http;
+
+namespace SubmissionMircroservice.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubmissionController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var filePath = CreateFilePath();
+
+            await using var writeStream = System.IO.File.Create(filePath);
+
+            foreach (var file in Request.Form.Files)
+            {
+                await file.CopyToAsync(writeStream);
+            }
+
+            return Ok();
+        }
+
+        private string CreateFilePath()
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), @"Uploads", finalString);
+            path += ".js";
+
+            return $"{path}";
+        }
+    }
+}
---
+++ SubmissionMicro/SubmissionMircroservice/SubmissionMircroservice/Controllers/SubmissionController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SubmissionMircroservice.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubmissionController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var filePath = CreateFilePath(".js");
+
+            await using var writeStream = System.IO.File.Create(filePath);
+
+            foreach (var file in Request.Form.Files)
+            {
+                await file.CopyToAsync(writeStream);
+            }
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Creates a randomly generated file name to store in 'Uploads' with the given extension
+        /// </summary>
+        /// <param name="extension">The extension to append to filepath</param>
+        /// <returns></returns>
+        private string CreateFilePath(string extension)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), @"Uploads", finalString);
+            path += extension;
+
+            return $"{path}";
+        }
+    }
+}
